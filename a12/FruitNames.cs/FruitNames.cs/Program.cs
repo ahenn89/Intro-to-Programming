50f8a8192616fe6227019cf91be25047ab10229a
ahenn89 <31427701+ahenn89@users.noreply.github.com>
2019-01-12 10:07:26 -0600

    Add files via upload

---
+++ a12/FruitNames.cs/FruitNames.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+class FruitNames
+{
+    static void Main()
+    {
+        StreamReader sr = new StreamReader("fruits.txt");
+        StreamWriter sw = new StreamWriter("fruits2.txt");
+
+        string line = sr.ReadLine();
+        string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(line.ToLower());
+
+
+        char cutter = ',';
+        string[] splitter = titleCase.Split(cutter);
+
+        Array.Sort(splitter);
+        var substring = string.Join(",", splitter);
+
+        sw.WriteLine(substring);
+
+        sr.Close();
+        sw.Close();
+        Console.ReadKey();
+
+    }
+}
